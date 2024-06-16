95c3033fb0b6f6341fc0772a7e897694be43aafa
GuiCastelo <guilherme.castelo@usp.br>
2024-06-16 20:36:08 -0300

    adiciona sinal cop_if

--- estagio_if.vhd
+++ estagio_if.vhd
@@ -49,7 +49,9 @@ architecture behave of estagio_if is
         );
     end component;
 
-    signal pc, pc_plus4, ri_if, ram_out : std_logic_vector(31 downto 0) := x"00000000";
+    signal pc_if, pc_plus4, ri_if, ram_out : std_logic_vector(31 downto 0) := x"00000000";
+
+    signal COP_if: instruction_type;
 
     signal hazard_nop: std_logic;
 
@@ -57,18 +59,36 @@ architecture behave of estagio_if is
 
 begin
     hazard_nop <= id_Branch_nop or id_hd_hazard;
-    pc_plus4 <= pc + x"00000004";
+    pc_plus4 <= pc_if + x"00000004";
     ri_if <= ram_out when hazard_nop = '0' else
              x"00000000";
+
+    COP_if <= ADD when (ri_if(14 downto 12) = "000" and ri_if(6 downto 0) = "0110011") else
+              SLT when (ri_if(14 downto 12) = "010" and ri_if(6 downto 0) = "0110011") else
+              ADDI when (ri_if(14 downto 12) = "000" and ri_if(6 downto 0) = "0010011") else
+              SLTI when (ri_if(14 downto 12) = "010" and ri_if(6 downto 0) = "0010011") else
+              SLLI when (ri_if(14 downto 12) = "001" and ri_if(6 downto 0) = "0010011") else
+              SRLI when (ri_if(31 downto 25) = "0000000" and ri_if(14 downto 12) = "101" and ri_if(6 downto 0) = "0010011") else
+              SRAI when (ri_if(31 downto 25) = "0100000" and ri_if(14 downto 12) = "101" and ri_if(6 downto 0) = "0010011") else
+              LW when (ri_if(14 downto 12) = "010" and ri_if(6 downto 0) = "0000011") else
+              SW when (ri_if(14 downto 12) = "010" and ri_if(6 downto 0) = "0100011") else
+              BEQ when (ri_if(14 downto 12) = "000" and ri_if(6 downto 0) = "1100011") else
+              BNE when (ri_if(14 downto 12) = "001" and ri_if(6 downto 0) = "0000000") else
+              BLT when (ri_if(14 downto 12) = "100" and ri_if(6 downto 0) = "0000000") else
+              HALT when (ri_if = x"0000006F") else
+              JAL when (ri_if(14 downto 12) = "000" and ri_if(6 downto 0) = "1101111") else
+              JALR when (ri_if(14 downto 12) = "000" and ri_if(6 downto 0) = "1100111") else
+              NOP when (ri_if = x"00000000") else
+              NOINST;
 
     process(clock, keep_simulating)
     begin
         if (keep_simulating = True) and (clock'event and clock = '1') then
             if (hazard_nop = '0' or id_PC_Src = '1') then
                 if (id_PC_Src = '1') then
-                    pc <= id_Jump_PC;
+                    pc_if <= id_Jump_PC;
                 elsif (id_PC_Src = '0') then
-                    pc <= pc_plus4;
+                    pc_if <= pc_plus4;
                 end if;
             end if;
 
@@ -76,7 +96,7 @@ begin
                 stop_simulation <= '1';
             end if;
 
-            BID <= pc & ri_if;
+            BID <= pc_if & ri_if;
         end if;
     end process;
 
@@ -89,7 +109,7 @@ begin
         port map(
             clock 	 => clock,								
             write 	 => '0',							
-            address  => pc,
+            address  => pc_if,
             data_in  => x"00000000",
             data_out => ram_out
         );
